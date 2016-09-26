b628d8be06d9ae900d3aef5452cb071f8ade3c62
Eliezer Wohl <eliezerwohl@gmail.com>
2016-09-26 16:13:17 -0400

    added whole site to wordpress.  need to make it so user can edit

--- libertyvice/wp-content/themes/libertyvice/header.php
+++ libertyvice/wp-content/themes/libertyvice/header.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://bootswatch.com/readable/bootstrap.min.css">
 <link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" type="text/css" href="assets/styles/styles.css">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
   <title>Liberty Vice</title>
   <?php wp_head(); ?>
 </head>
+
