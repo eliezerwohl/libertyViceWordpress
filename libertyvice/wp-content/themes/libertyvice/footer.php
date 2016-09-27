7ffa7562a78a0b517b3fceba5e38e8c843ddf9f2
Eliezer Wohl <eliezerwohl@gmail.com>
2016-09-27 13:48:52 -0400

    localJS

--- libertyvice/wp-content/themes/libertyvice/footer.php
+++ libertyvice/wp-content/themes/libertyvice/footer.php
@@ -10,13 +10,13 @@
  */
 ?>
 
-		</div><!-- .site-content -->
+
 
 		
 
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/javascript/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/javascript/mainpage.js"></script>
 <?php wp_footer(); ?>
 </body>
